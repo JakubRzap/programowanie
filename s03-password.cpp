a653bb165dd6322cb9e5c98ec61ec9db497ccb1e
JakubRzap <91731067+JakubRzap@users.noreply.github.com>
2022-01-14 23:30:52 +0100

    Update s03-password.cpp

--- s03-password.cpp
+++ s03-password.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
+
 #include <string>
 
-int main(int argc, char* argv[])
-{
-    if (argc == 0) {
-        return 1;
-    }
+int main(int argc, char * argv[]) {
+  if (argc == 0) {
+    return 1;
+  }
 
-    std::string password = std::string{argv[1]};
-    while (true){
-        std::cout << "password: " << "\n";
-        std::string conf_pass;
-        std::cin >> conf_pass;
-        if (conf_pass == password){
-            break;
-        }
+  std::string password = std::string {
+    argv[1]
+  };
+  while (true) {
+    std::cout << "password: " << "\n";
+    std::string conf_pass;
+    std::cin >> conf_pass;
+    if (conf_pass == password) {
+      break;
     }
-    return 0;
+  }
+  return 0;
 }
