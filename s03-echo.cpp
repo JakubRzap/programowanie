0c8e1cae723cb7d9888241d44521b33e768b6358
JakubRzap <91731067+JakubRzap@users.noreply.github.com>
2022-01-14 23:33:07 +0100

    Update s03-echo.cpp

--- s03-echo.cpp
+++ s03-echo.cpp
@@ -1,34 +1,35 @@
 #include <iostream>
+
 #include <string>
 
-int main(int argc, char* argv[]){
-    if (argc == 0) {
-        return 1;
+int main(int argc, char * argv[]) {
+  if (argc == 0) {
+    return 1;
+  }
+  if (argv[1] == std::string("-n")) {
+    for (int i = 2; i <= argc; i++) {
+      std::cout << argv[i] << " ";
     }
-    if (argv[1] == std::string("-n")){
-        for (int i = 2; i <= argc; i++){
-            std::cout << argv[i] << " ";
-        }
-        std::cout << '\n';
-    } else if (argv[1] == std::string("-l")){
-        for (int i = 2; i <= argc; i++){
-            std::cout << argv[i] << '\n';
-        }
-    } else if (argv[1] == std::string("-r")){
-        if (argv[2] == std::string("-n")){
-            for (int i = argc - 1; i > 2; i--){
-                std::cout << argv[i] << " ";
-            }
-            std::cout << '\n';
-        } else if (argv[2] == std::string("-l")){
-            for (int i = argc - 1; i > 2; i--){
-                std::cout << argv[i] << '\n';
-            }
-        } else {
-            for (int i = argc - 1; i > 1; i--){
-                std::cout << argv[i] << "\n";
-            }
-        }
+    std::cout << '\n';
+  } else if (argv[1] == std::string("-l")) {
+    for (int i = 2; i <= argc; i++) {
+      std::cout << argv[i] << '\n';
     }
-    return 0;
+  } else if (argv[1] == std::string("-r")) {
+    if (argv[2] == std::string("-n")) {
+      for (int i = argc - 1; i > 2; i--) {
+        std::cout << argv[i] << " ";
+      }
+      std::cout << '\n';
+    } else if (argv[2] == std::string("-l")) {
+      for (int i = argc - 1; i > 2; i--) {
+        std::cout << argv[i] << '\n';
+      }
+    } else {
+      for (int i = argc - 1; i > 1; i--) {
+        std::cout << argv[i] << "\n";
+      }
+    }
+  }
+  return 0;
 }
